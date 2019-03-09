a8943fa86f4903519207af2f0e614d6adc5e090e
root <root@cdocker01.tatanka.local>
2019-03-09 22:19:19 +0100

    general changes

--- conf/setup_dvwa.sh
+++ conf/setup_dvwa.sh
@@ -7,5 +7,7 @@ sed -i "s/$_DVWA[ 'recaptcha_public_key' ] = ''/$_DVWA[ 'recaptcha_public_key' ]
 sed -i "s/= 'impossible'/ = 'low'/g" /app/config/config.inc.php && \
 sed -i 's/FileInfo/All/g' /etc/apache2/sites-available/000-default.conf && \
 sed -i 's/root/admin/g' /app/config/config.inc.php && \
-echo "sed -i \"s/p@ssw0rd/\$PASS/g\" /app/config/config.inc.php" >> /create_mysql_admin_user.sh && \
+# echo "sed -i \"s/p@ssw0rd/\$PASS/g\" /app/config/config.inc.php" >> /create_mysql_admin_user.sh && \
 echo 'session.save_path = "/tmp"' >> /etc/php5/apache2/php.ini
+mysqldump --user=admin --password=p@ssw0rd --host=127.0.0.1 dvwa < /tmp/dvwa.mysql
+
