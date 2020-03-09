fd7643a69dee0745161c9a651768b99fb8073fe0
aracloud <aracloud@gmx.net>
2020-03-09 17:12:21 +0100

    update

--- conf/login.php
+++ conf/login.php
@@ -114,6 +114,14 @@ echo "
 print "<div align=center><font size=5>";
 echo "Server IP: ";
 echo $_SERVER['SERVER_ADDR'];
+echo "<br />";
+echo "<br />";
+echo "Client/Proxy IP: ";
+echo $_SERVER['REMOTE_ADDR'];
+echo "<br />";
+echo "<br />";
+echo "X44 IP: ";
+echo $_SERVER['HTTP_X_FORWARDED_FOR'];
 print "</font></div>";
 
 echo "
